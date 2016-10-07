8156528bf7f954291fa04cbdba61a0d3061270e2
PashaGeronimo <pasha@pasha.kiev.ua>
2016-10-07 23:05:27 +0300

    Непрерывное движение ленты

--- old/slide_pg.js
+++ old/slide_pg.js
@@ -1,6 +1,6 @@
-(function ($) {
+(function($) {
 
-    $.fn.slide_pg = function (opt) {
+    $.fn.slide_pg = function(opt) {
 
         var options = $.extend({
             photos: 10,
@@ -14,10 +14,10 @@
         }, opt);
 
 
-        var make = function () {
+        var make = function() {
 
             //кадр по центру в начале
-            var MoveLeft = -(options.centerFrame * options.widthPic - 900 / 2 + (options.marginImg * 2) - options.widthPic / 2);
+            var MoveLeft = -(options.centerFrame * options.widthPic - 900 / 2 + (options.marginImg * 2) - options.widthPic / 2); 
             $('#lenta').css({
                 'left': MoveLeft + 'px',
             });
@@ -41,45 +41,35 @@
             // длина ленты вычитая видимую часть + margin* кол фото*2
             var widthLenta = options.widthPic * (options.photos) - $('#container').css('width').slice(0, -2) + options.marginImg * (options.photos * 2);
 
-            var stop = 1;
-            // движение ленты вправо
-            function my() {
-                if (MoveLeft <= -widthLenta) stop = 0;
-                if (stop == 1) {
-                    MoveLeft = MoveLeft - options.speed;
-                    $('#lenta').css({
-                        'left': MoveLeft + 'px',
-                    });
-                    setTimeout(my, 0.1);
-                }
-            }
-            $('#right').mousedown(function () {
-                stop = 1;
-                my();
-            });
-            $('#right').mouseup(function () {
-                stop = 0;
-            });
-
-
             // движение ленты влево
-            function myL() {
-                if (MoveLeft >= 0) stop = 0;
-                if (stop == 1) {
-                    MoveLeft = MoveLeft + options.speed;
-                    $('#lenta').css({
-                        'left': MoveLeft + 'px',
-                    });
-                    setTimeout(myL, 0.1);
-                }
-            }
-            $('#left').mousedown(function () {
-                stop = 1;
-                myL();
+            $('#left').click(function() {
+                if (MoveLeft > -options.lentaStep) {
+                    MoveLeft = 0;
+                } else {
+                    MoveLeft += options.lentaStep;
+                };
+                $('#lenta').animate({
+                    left: MoveLeft + 'px',
+                }, options.speed);
+
             });
-            $('#left').mouseup(function () {
-                stop = 0;
+
+
+            // движение ленты вправо
+
+            $('#right').click(function() {
+                if ((widthLenta + MoveLeft) < options.lentaStep) {
+                    MoveLeft = -widthLenta;
+                } else {
+                    MoveLeft -= options.lentaStep;
+                };
+                $('#lenta').animate({
+                    left: MoveLeft + 'px',
+                }, options.speed);
             });
+
+
+
 
 
 
@@ -115,4 +105,4 @@
         return this.each(make) // this - возвращает то, к чему применяется функция testPlug ( см. index.html )
     }
 
-} (jQuery))
+}(jQuery))
