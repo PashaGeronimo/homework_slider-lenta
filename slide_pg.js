20bed2e92131bf284abd89a5f509a20b961b4c1a
PashaGeronimo <pasha@pasha.kiev.ua>
2016-10-07 13:41:17 +0300

    Сделал анимацию и остановку по краям

--- slide_pg.js
+++ slide_pg.js
@@ -7,68 +7,86 @@
             namefile: 'name-',
             lentaStep: 100,
             widthPic: 270,
+            marginImg: 2,
+            speed: 400,
+            centerFrame: 4,
 
         }, opt);
 
 
         var make = function() {
-            // получаем значение css left
-            var MarginL = +$('#lenta').css('left').slice(0, -2); // обрезаем px
+
+            //кадр по центру в начале
+            var MoveLeft = -(options.centerFrame * options.widthPic - 900 / 2 + (options.marginImg * 2) - options.widthPic / 2); 
+            $('#lenta').css({
+                'left': MoveLeft + 'px',
+            });
 
             // рисуем ленту
-            for (i = 1; i < options.photos; i++) {
+            for (i = 1; i <= options.photos; i++) {
                 $('#lenta').append('<img src="img/' + options.namefile + i + '.jpg" id="img' + i + '" >');
-
             };
 
-            //параметры каждой картинки
+            //параметры каждой картинки 
             $('[id *=img ]').css({
                 'width': options.widthPic + 'px',
-                'margin': '2px',
+                'margin': options.marginImg + 'px',
             });
 
             // высота обрезки
             $('#container').css({
                 'height': options.widthPic / 1.85 + 'px',
             });
-            
-            // длина ленты
-            var widthAll = $('#img5').innerWidth() * options.photos;
 
-            // нажатие влево
+            // длина ленты вычитая видимую часть + margin* кол фото*2
+            var widthLenta = options.widthPic * (options.photos) - $('#container').css('width').slice(0, -2) + options.marginImg * (options.photos * 2);
+
+            // движение ленты влево
             $('#left').click(function() {
-                MarginL += options.lentaStep;
+                if (MoveLeft > -options.lentaStep) {
+                    MoveLeft = 0;
+                } else {
+                    MoveLeft += options.lentaStep;
+                };
                 $('#lenta').animate({
-                    left: MarginL + 'px',
-                }, 400);
-                console.log(MarginL);
+                    left: MoveLeft + 'px',
+                }, options.speed);
+
             });
 
-            // нажатие вправо
+
+            // движение ленты вправо
+
             $('#right').click(function() {
-                MarginL -= options.lentaStep;
+                if ((widthLenta + MoveLeft) < options.lentaStep) {
+                    MoveLeft = -widthLenta;
+                } else {
+                    MoveLeft -= options.lentaStep;
+                };
                 $('#lenta').animate({
-                    left: MarginL + 'px',
-                }, 400);
-                      console.log(MarginL);
+                    left: MoveLeft + 'px',
+                }, options.speed);
             });
 
-            console.log(options.widthPic);
 
-           // Регулировка размера кнопок
+
+
+
+
+            // Регулировка размера кнопок
             $('.buttons').css({
-                'top':  +$('#container').css('top').slice(0, -2) + 39 - ((options.widthPic-150)/1.85) - 120 + 'px',
+                'top': +$('#container').css('top').slice(0, -2) + 39 - ((options.widthPic - 150) / 1.85) - 120 + 'px',
             });
 
-            var sizbtn = 40 + ((options.widthPic-150)/3.6);
+            var sizbtn = 40 + ((options.widthPic - 150) / 3.6);
             $('#right').css({
-            'border-top': ''+sizbtn+'px solid transparent',
-            'border-bottom': ''+sizbtn+'px solid transparent',
+                'border-top': '' + sizbtn + 'px solid transparent',
+                'border-bottom': '' + sizbtn + 'px solid transparent',
             });
 
             $('#left').css({
-            'border-top': ''+sizbtn+'px solid transparent',
-            'border-bottom': ''+sizbtn+'px solid transparent',
+                'border-top': '' + sizbtn + 'px solid transparent',
+                'border-bottom': '' + sizbtn + 'px solid transparent',
             });
 
 
